2021e78a9da77c49aa80f394b0cfd2cb762da044
Yusup <b_usup@mail.ru>
2023-07-20 09:15:10 +0300

    добавил комментарии и докстринги

--- network/serializers.py
+++ network/serializers.py
@@ -1,50 +1,57 @@
 from rest_framework import serializers
 from .models import Contact, Product, NetworkNode
 
-# Сериализатор для модели Contact
+
 class ContactSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Contact.
+    """
     class Meta:
         model = Contact
         fields = '__all__'
 
 
-# Сериализатор для модели Product
 class ProductSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для модели Product.
+    """
     class Meta:
         model = Product
         fields = '__all__'
 
 
-# Сериализатор для модели NetworkNode
 class NetworkNodeSerializer(serializers.ModelSerializer):
-    contacts = ContactSerializer()  # Вложенный сериализатор для связи с моделью Contact
-    products = ProductSerializer(many=True)  # Вложенный сериализатор для связи с моделью Product (разрешает несколько продуктов)
+    """
+    Сериализатор для модели NetworkNode.
+
+    Включает в себя вложенные сериализаторы для связанных моделей Contact и Product.
+    """
+
+    contacts = ContactSerializer()
+    products = ProductSerializer(many=True)
 
     class Meta:
         model = NetworkNode
         fields = '__all__'
 
     def create(self, validated_data):
-        # Получаем данные для поля contacts и products
+        """
+        Метод для создания экземпляра NetworkNode.
+
+        Parameters:
+            validated_data (dict): Валидированные данные запроса.
+
+        Returns:
+            NetworkNode: Созданный экземпляр NetworkNode.
+        """
         contacts_data = validated_data.pop('contacts')
         products_data = validated_data.pop('products')
 
         # Создаем экземпляр контакта (Contact)
-        contacts_serializer = ContactSerializer(data=contacts_data)
-        if contacts_serializer.is_valid():
-            contacts = contacts_serializer.save()
-        else:
-            raise serializers.ValidationError(contacts_serializer.errors)
+        contacts = Contact.objects.create(**contacts_data)
 
         # Создаем список продуктов (Product)
-        products = []
-        for product_data in products_data:
-            product_serializer = ProductSerializer(data=product_data)
-            if product_serializer.is_valid():
-                product = product_serializer.save()
-                products.append(product)
-            else:
-                raise serializers.ValidationError(product_serializer.errors)
+        products = [Product.objects.create(**product_data) for product_data in products_data]
 
         # Создаем экземпляр сетевого звена (NetworkNode)
         network_node = NetworkNode.objects.create(contacts=contacts, **validated_data)
--- network/views.py
+++ network/views.py
@@ -5,30 +5,56 @@ from .serializers import ContactSerializer, ProductSerializer, NetworkNodeSerial
 
 
 class ContactViewSet(viewsets.ModelViewSet):
+    """
+    Представление для операций CRUD с моделью Contact.
+    """
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    """
+    Представление для операций CRUD с моделью Product.
+    """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class NetworkNodeViewSet(viewsets.ModelViewSet):
+    """
+    Представление для операций CRUD с моделью NetworkNode.
+    """
     queryset = NetworkNode.objects.all()
     serializer_class = NetworkNodeSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['contacts__country']
 
     def update(self, request, *args, **kwargs):
+        """
+        Пользовательский метод обновления сетевого звена.
+
+        При обновлении запроса, исключает поле 'supplier' из данных запроса.
+
+        Parameters:
+            request (Request): Входящий запрос.
+            *args: Позиционные аргументы.
+            **kwargs: Именованные аргументы.
+
+        Returns:
+            Response: Ответ на запрос обновления сетевого звена с обновленными данными.
+        """
         # Создаем копию словаря с данными запроса
+        # Создание копии предотвращает изменение исходных данных, чтобы не вносить изменения в данные запроса напрямую.
         mutable_data = request.data.copy()
 
         # Проверяем, содержится ли 'supplier' в mutable_data и удаляем его
+        # Это выполняется потому, что мы хотим обновить сетевое звено без изменения поля 'supplier', если оно было передано в запросе.
         if 'supplier' in mutable_data:
             del mutable_data['supplier']
 
         # Создаем новый объект QueryDict с обновленными данными
+        # QueryDict - это специальный класс Django, используемый для представления данных запроса с параметрами, переданными в URL или теле запроса.
+        # Мы обновляем новый QueryDict данными из mutable_data, которые были отфильтрованы на предыдущем шаге. Теперь updated_query_dict содержит все данные, полученные из запроса за исключением поля 'supplier'.
         updated_query_dict = QueryDict('', mutable=True)
         updated_query_dict.update(mutable_data)
 
